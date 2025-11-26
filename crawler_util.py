856acd789e771c201782c9985fd60186be0fc3b8
jiuzixue09 <jiuzixue09@hotmail.com>
2025-11-26 12:59:33 +0800

    code optimization

--- crawler_util.py
+++ crawler_util.py
@@ -14,7 +14,7 @@ user_agent = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0',
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
     ]
-headless = False
+headless = True
 
 def get_random_user_agent():
     return user_agent[random.randrange(len(user_agent))]
